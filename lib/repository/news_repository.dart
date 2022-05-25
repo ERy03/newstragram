4b242ff753e8981a0c92b2dcecfa2e663dc1628b
Ryota Enoki <ryoenoki2013@gmail.com>
2022-05-25 11:28:29 +0900

    utilizing news api

--- lib/repository/news_repository.dart
+++ lib/repository/news_repository.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:newstragram/data/category_info.dart';
 import 'package:newstragram/data/search_type.dart';
+import 'package:newstragram/models/model/news_model.dart';
 
 
 class NewsRepository {
-  Future<void> getNews({required SearchType searchType, String? keyword, Category? category}) async{
+
+  static const BASE_URL = "https://newsapi.org/v2/top-headlines?country=us";
+  static const API_KEY = "c27b14cba77046f984200c8c0e486afb";
+
+  Future<Article> getNews({required SearchType searchType, String? keyword, Category? category}) async{
 
   }
 }
