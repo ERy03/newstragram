f72e051ea0845d74297c8b837c7ed5776140a0e2
Ryota Enoki <ryoenoki2013@gmail.com>
2022-05-25 11:57:16 +0900

    news api

--- lib/repository/news_repository.dart
+++ lib/repository/news_repository.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:newstragram/data/category_info.dart';
 import 'package:newstragram/data/search_type.dart';
 import 'package:newstragram/models/model/news_model.dart';
@@ -9,7 +12,37 @@ class NewsRepository {
   static const BASE_URL = "https://newsapi.org/v2/top-headlines?country=us";
   static const API_KEY = "c27b14cba77046f984200c8c0e486afb";
 
-  Future<Article> getNews({required SearchType searchType, String? keyword, Category? category}) async{
+  Future<List<Article>> getNews({
+    required SearchType searchType,
+    String? keyword,
+    Category? category
+  }) async{
 
+    List<Article> results = [];
+
+    http.Response? response;
+
+    switch (searchType) {
+      case SearchType.HEAD_LINE:
+        final requestUrl = Uri.parse(BASE_URL + "&apiKey=$API_KEY");
+        response = await http.get(requestUrl);
+        break;
+      case SearchType.KEYWORD:
+        final requestUrl = Uri.parse(BASE_URL + "&q=$keyword&apiKey=$API_KEY");
+        response = await http.get(requestUrl);
+        break;
+      case SearchType.CATEGORY:
+        final requestUrl = Uri.parse(BASE_URL + "&category=${category?.nameEn}&apiKey=$API_KEY");
+        response = await http.get(requestUrl);
+        break;
+    }
+
+    if (response.statusCode == 200) {
+      final responseBody = response.body;
+      results = News.fromJson(jsonDecode(responseBody)).articles;
+    } else {
+      throw Exception("falied to load news");
+    }
+    return results;
   }
 }
