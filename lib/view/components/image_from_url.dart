6b256c83193650058d7931f39f2b5156218bb123
Ryota Enoki <ryoenoki2013@gmail.com>
2022-05-26 15:27:14 +0900

    image from url

---
+++ lib/view/components/image_from_url.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ImageFromUrl extends StatelessWidget {
+  final String imageUrl;
+  const ImageFromUrl({required this.imageUrl});
+
+  @override
+  Widget build(BuildContext context) {
+    return
+  }
+}
