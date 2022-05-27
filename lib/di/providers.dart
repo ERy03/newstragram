d75300f01d9c568482923cfe94b13f726fc561de
Ryota Enoki <ryoenoki2013@gmail.com>
2022-05-27 17:01:33 +0900

    dependency injection

---
+++ lib/di/providers.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+List globalProviders = [
+  ...independentModels,
+  ...dependentModels,
+  ...viewModels
+];
+
+List independentModels = [
+
+];
