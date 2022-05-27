90f9db367df1f8a94dbe0d158724d8104712da45
Ryota Enoki <ryoenoki2013@gmail.com>
2022-05-27 12:11:15 +0900

    drift database

--- lib/models/db/database.dart
+++ lib/models/db/database.dart
@@ -3,5 +3,11 @@ import 'package:drift/drift.dart';
 part 'database.g.dart';
 
 class ArticleRecords extends Table {
+  TextColumn get title => text()();
+  TextColumn get description => text()();
+  TextColumn get url => text()();
+  TextColumn get urlToImage => text()();
+  TextColumn get publishDate => text()();
+  TextColumn get content => text()();
 
 }
