5c3f2092ae82adbd61375da3972ed849c986c6e4
Ryota Enoki <ryoenoki2013@gmail.com>
2022-05-27 15:32:03 +0900

    about us page animation

--- lib/view/screens/pages/about_us_page.dart
+++ lib/view/screens/pages/about_us_page.dart
@@ -8,11 +8,33 @@ class AboutUsPage extends StatefulWidget {
 }
 
 class _AboutUsPageState extends State<AboutUsPage> {
+  bool _selected = false;
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Container(),
+        body: Center(
+          child: AnimatedContainer(
+            width: _selected ? 300 : 50,
+            height: _selected ? 200 : 25,
+            child: _selected
+              ? Text("Newstagram", style: TextStyle(fontSize: 40.0),)
+              : Text("Newstagram", style: TextStyle(fontSize: 6.0),),
+            alignment: _selected ? Alignment.center : Alignment.topCenter,
+            duration: Duration(milliseconds: 1000),
+            curve: Curves.fastOutSlowIn,
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.play_arrow),
+          tooltip: "飛び出すよ！",
+           onPressed: () {
+             setState(() {
+               _selected = !_selected;
+             });
+           },
+        ),
       ),
     );
   }
